78ab051fc9c42b952ceaaffc3b005b82f7b1205d
Bobby Neal <rsneal01@gmail.com>
2020-10-05 17:07:14 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
   
   def change
-    add_column :grade, :birthdate, :string
+    add_column :grade, :integer, :birthdate, :string
   
 end
